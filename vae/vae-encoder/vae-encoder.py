69be71a3327521c30f988f0c67ea31e8d7105dc8
Tran Tuan Dung <tuandung171206@gmail.com>
2026-07-02 07:51:32 +0700

    Solve vae-encoder

--- vae/vae-encoder/vae-encoder.py
+++ vae/vae-encoder/vae-encoder.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def vae_encoder(x: np.ndarray, W_mu: np.ndarray, b_mu: np.ndarray, W_logvar: np.ndarray, b_logvar: np.ndarray) -> dict:
-   
-
-    mu = np.dot(x, W_mu) + b_mu
-    log_var = np.dot(x, W_logvar) + b_logvar
-    
-    return {"mu": mu, "log_var": log_var}
+    """
+    Returns: dict with 'mu' and 'log_var' as np.ndarrays of shape (batch, latent_dim)
+    """
+    mu = np.matmul  ( x,W_mu) + b_mu 
+    log_var = np.matmul ( x,W_logvar ) + b_logvar 
+    return (mu , log_var  )
